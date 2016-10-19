eead9d08de2c173241a2a332f76f3313bdb443f4
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-19 01:37:41 -0500

    Create DB based CRUD fro GamesController

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using FrontendPractice.Data;
 using FrontendPractice.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,12 +10,42 @@ namespace FrontendPractice.Controllers
     [Route("/api/[controller]")]
     public class GamesController : Controller
     {
+        private PracticeContext _context;
+
+        public GamesController(PracticeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGetAttribute]
         public IEnumerable<Game> Get()
         {
-            return new List<Game>() {
-                new Game() { Id = new Guid(), Name = "Chess", Description = "A logic game for", MaximumPlayers = 2 },
-                new Game() { Id = new Guid(), Name = "Monopoly", Description = "Get rich, biatch", MaximumPlayers = 8 }
-            };
+            return _context.Games.ToList();
+        }
+
+        [HttpPostAttribute]
+        public IActionResult Post([FromBodyAttribute]Game game)
+        {
+            _context.Games.Add(game);
+            _context.SaveChanges();
+            return Created("", game);
+        }
+
+        [HttpPutAttribute]
+        public IActionResult Put([FromBodyAttribute] Game game)
+        {
+            _context.Games.Update(game);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDeleteAttribute]
+        public IActionResult Delete([FromBodyAttribute]string id)
+        {
+            var game = _context.Games.Where(g => g.Id == Guid.Parse(id)).FirstOrDefault();
+            _context.Games.Remove(game);
+            _context.SaveChanges();
+            return Delete(id);
         }
     }
 }
